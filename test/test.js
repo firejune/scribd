1cb6027f0a7a4d5cf204a534bf7378e6ef983e98
Junho Kyung <to@firejune.com>
2012-12-21 21:04:45 +0900

    update test suite

--- test/test.js
+++ test/test.js
@@ -32,107 +32,105 @@ var key = "ENTER-YOUR-API-KEY-HERE"
   }
   , units = []
   , tests = {
-    "docs": {
-      "upload": function(r) {
-        if (r.doc_id) params.docId = r.doc_id;
-        if (r.access_key) params.accessKey = r.access_key;
-        if (r.secret_password) params.secretPassword = r.secret_password;
-        params.access = null;
-        params.docType = 'pdf';
-        params.file = './test.jpg';
-        return r.doc_id && r.access_key && r.secret_password;
-      },
-      "uploadFromUrl": function(r) {
-        if (r.doc_id) params.docIds = r.doc_id;
-        return r.doc_id && r.access_key && !r.secret_password;
-      },
-      "getList": function(r) {
-        if (r && r.result && r.result.length) {
-          params.currentDocId = r.result[0].doc_id;
-        }
-        return r && r.result && !!r.result.length;
-      },
-      "getConversionStatus": function(r) {
-        return r == "PROCESSING" || r == "DONE" || r == "ERROR" || r == "DISPLAYABLE";
-      },
-      "getSettings": function(r) {
-        return params.accessKey == r.access_key
-            && params.secretPassword == r.secret_password;
-      },
-      "changeSettings": function(r) {
-        params.docType = null;
-        params._docId = params.docId;
-        if (params.currentDocId) params.docId = params.currentDocId;
-        return true;
-      },
-      "getDownloadUrl": function(r) {
-        params.docId = params._docId;
-        return "string" == typeof r;
-      },
-      "getStats": function(r) {
-        params.docId = params._docId;
-        return r && r.reads != undefined;
-      },
-      "delete": function(r) {
-        params.docId = params.docIds;
-        return true;
-      },
-      "search": function(r) {
-        return r && r.result && r.result.length == 1;
-      },
-      "getCategories": function(r) {
-        return r && r.result && !!r.result.length;
-      },
-      "featured": function(r) {
-        return r && r.result && r.result.length == 2;
-      },
-      "browse": function(r) {
-        return r && r.result && r.result.length == 2;
-      },
-      "uploadThumb": function(r) {
-        return true;
-      }
+    "docs.upload": function(r) {
+      if (r.doc_id) params.docId = r.doc_id;
+      if (r.access_key) params.accessKey = r.access_key;
+      if (r.secret_password) params.secretPassword = r.secret_password;
+      params.access = null;
+      params.docType = 'pdf';
+      params.file = './test.jpg';
+      return r.doc_id && r.access_key && r.secret_password;
     },
-    "thumbnail": {
-      "get": function(r) {
-        return "string" == typeof r;
-      }
+    "docs.uploadFromUrl": function(r) {
+      if (r.doc_id) params.docIds = r.doc_id;
+      return r.doc_id && r.access_key && !r.secret_password;
     },
-    "user": {
-      "login": function(r) {
-        return r.session_key && r.user_id && r.username;
-      },
-      "signup": function(r) {
-        return r.session_key && r.user_id && r.username;
-      },
-      "getAutoSigninUrl": function(r) {
-        return "string" == typeof r;
+    "docs.getList": function(r) {
+      if (r && r.result && r.result.length) {
+        params.currentDocId = r.result[0].doc_id;
       }
+      return r && r.result && !!r.result.length;
+    },
+    "docs.getConversionStatus": function(r) {
+      return r == "PROCESSING" || r == "DONE" || r == "ERROR" || r == "DISPLAYABLE";
+    },
+    "docs.getSettings": function(r) {
+      return params.accessKey == r.access_key
+          && params.secretPassword == r.secret_password;
+    },
+    "docs.changeSettings": function(r) {
+      params.docType = null;
+      params._docId = params.docId;
+      if (params.currentDocId) params.docId = params.currentDocId;
+      return true;
+    },
+    "docs.getDownloadUrl": function(r) {
+      params.docId = params._docId;
+      return "string" == typeof r;
+    },
+    "docs.getStats": function(r) {
+      params.docId = params._docId;
+      return r && r.reads != undefined;
+    },
+    "docs.delete": function(r) {
+      params.docId = params.docIds;
+      return true;
+    },
+    "docs.search": function(r) {
+      return r && r.result && r.result.length == 1;
+    },
+    "docs.getCategories": function(r) {
+      return r && r.result && !!r.result.length;
+    },
+    "docs.featured": function(r) {
+      return r && r.result && r.result.length == 2;
+    },
+    "docs.browse": function(r) {
+      return r && r.result && r.result.length == 2;
+    },
+    "docs.uploadThumb": function(r) {
+      return true;
+    },
+    "thumbnail.get": function(r) {
+      return "string" == typeof r;
+    },
+    "user.login": function(r) {
+      return r.session_key && r.user_id && r.username;
+    },
+    "user.signup": function(r) {
+      return r.session_key && r.user_id && r.username;
+    },
+    "user.getAutoSigninUrl": function(r) {
+      return "string" == typeof r;
     }
   };
 
 
 /**
- * Align test units form 'methods'
+ * Align test units form 'tests'
  */
 
-Object.keys(methods).forEach(function(model) {
-  Object.keys(methods[model]).forEach(function(method) {
-    var func = methods[model][method] + ''
-      , pre = func.split('function (options, ')[1] || ''
-      , args = pre.split(') {\n')[0];
-      
-    units.push({
-        model: model
-      , method: method
-      , args: args
-      , ok: tests[model] && tests[model][method] || function() {}
-    });
-  });   
+Object.keys(tests).forEach(function(key) {
+
+  var model = key.split('.')[0]
+    , method = key.split('.')[1]
+    , func = methods[model][method] + ''
+    , pre = func.split('function (options, ')[1] || ''
+    , args = pre.split(') {\n')[0];
+
+  units.push({
+      model: model
+    , method: method
+    , args: args
+    , ok: tests[model + "." + method] || function() {}
+  });
+
 });
+
 
 units.reverse();
 run();
+
 
 function run() {
   var test = units.pop()
@@ -148,7 +146,7 @@ function run() {
           clog.info('ok');
           success++;
         } else {
-          clog.warn('not ok');
+          clog.warn('not ok', res);
           failure++;
         }
       }
